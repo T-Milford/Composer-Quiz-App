d1c5b593393f8970b94a02c58532f2407d864355
Tobie Milford <tobie.milford@gmail.com>
2019-08-04 11:48:07 -0700

    'for Aleks'

---
+++ bank.js
@@ -0,0 +1,34 @@
+const questionBank = [
+    {
+      question: 'Which composer, on his death bed, famously had a friend burn all of his unreleased music?',
+      answers: [
+        'Brahms',
+        'Beethoven',
+        'Bach',
+        'Mozart'
+        ],
+      correctAnswer: 'Brahms'
+    },
+    {
+      question: 'Which composer was happy to go to jail for several days because he refused to give an \
+      initial ‘honorary’ payment to his new prospective employer?',
+      answers: [
+        'Brahms',
+        'Beethoven',
+        'Bach',
+        'Mozart'
+        ],
+      correctAnswer: 'Bach'
+    }, 
+    {
+    question: 'Which composer wrote “Air on a G String”?',
+      answers: [
+        'Brahms',
+        'Beethoven',
+        'Bach',
+        'Mozart'
+        ],
+      correctAnswer: 'Bach'
+    },
+];
+
