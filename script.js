836dd9a72591ef13a80566ba71e69123a65c8d14
Tobie Milford <tobie.milford@gmail.com>
2019-08-06 19:49:00 -0700

    'for Slack question re: undefined submit'

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+// Users should be prompted through a series of at least 5 multiple choice questions that they can answer.
+// Users should be asked questions 1 after the other.
+// Users should only be prompted with 1 question at a time.
+// Users should not be able to skip questions.
+    // Users will see which question they're on (for instance, "7 out of 10")
+    // and their current score ("5 correct, 2 incorrect").
+
 let questionCounter = 0;
 let scoreCounter = 0;
 
@@ -9,32 +16,53 @@ function afterFirstClick() {
     $('.js-intro').on('click', '.begin_button', function(event) {
         console.log('begin_button clicked!');
         $('.js-intro').remove();
+        questionDisplayer();
     })
 }
 
-// Users should be prompted through a series of at least 5 multiple choice questions that they can answer.
-// Users should be asked questions 1 after the other.
-// Users should only be prompted with 1 question at a time.
-// Users should not be able to skip questions.
-    // Users will see which question they're on (for instance, "7 out of 10")
-    // and their current score ("5 correct, 2 incorrect").
+// to do:    
+//      questionBank[insert questionCounter here]
 
-// this function will render the questions and answers using HTML and data from bank.js, 
-// it will insert it into .js-main_window
-// it will then send users to a correctAnswer or incorrectAnswer function
-
+//      determine how to submit answer
+//      compare user's answer to actual answer
+//      it will then send users to a correctAnswer or incorrectAnswer function
 function questionDisplayer () {
     $('.question_container').html(
-        `<div class="question_formatter">
+        `
+        <div class="question_formatter">
             <h2>${questionBank[1].question}</h2>
-            console.log(<h2>${questionBank[1][1]}</h2>);
-        </div>`
+        </div>
+
+        <form class="answer_container"> 
+            <label class="answer_formatter">
+                <input type="radio" value="${questionBank[0].answers[0]}" name="answer" required>
+                <span>${questionBank[1].answers[0]}</span> 
+                </label>
+            <label class="answer_formatter">
+                <input type="radio" value="${questionBank[0].answers[1]}" name="answer" required>
+                <span>${questionBank[1].answers[1]}</span> 
+                </label>
+            <label class="answer_formatter">
+                <input type="radio" value="${questionBank[0].answers[2]}" name="answer" required>
+                <span>${questionBank[1].answers[2]}</span> 
+                </label>
+            <label class="answer_formatter">
+                <input type="radio" value="${questionBank[0].answers[3]}" name="answer" required>
+                <span>${questionBank[1].answers[3]}</span> 
+                </label>
+            <button class="answer_submit" type="submit">Check your answer</button>
+        </form>
+        `
     )
-    console.log('questionDisplayer ran');
 }
 
 function respondToAnswer () {
-
+    $('.question_container').on(submit, function () {
+        event.preventDefault();
+        console.log('respondToAnswer ran!');
+        // if ().val() == 
+    }
+        )
 
 }
 
@@ -60,7 +88,8 @@ function userSelectAnswer() {
 
 function handleAllFunctions () {
     afterFirstClick();
-    questionDisplayer();
+    respondToAnswer();
+    
     
 }
 
